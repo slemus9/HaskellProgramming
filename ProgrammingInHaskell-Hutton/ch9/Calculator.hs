adf9859a526ae7cb93ce8ff4d63ea764060f261b
Sebastián Lemus <sebastian.slc9@gmail.com>
2020-01-08 13:15:17 -0500

    ProgrammingInHaskell-Hutton: chapter 9 calculator completed

---
+++ ProgrammingInHaskell-Hutton/ch9/Calculator.hs
@@ -0,0 +1,70 @@
+import Notes9
+import ArithmeticParser
+
+-- * Calculator
+box :: [String ]
+box = ["+---------------+",
+       "|               |",
+       "+---+---+---+---+",
+       "| q | c | d | = |",
+       "+---+---+---+---+",
+       "| 1 | 2 | 3 | + |",
+       "+---+---+---+---+",
+       "| 4 | 5 | 6 | - |",
+       "+---+---+---+---+",
+       "| 7 | 8 | 9 | * |",
+       "+---+---+---+---+",
+       "| 0 | ( | ) | / |",
+       "+---+---+---+---+"]
+
+buttons :: [Char]
+buttons = standard ++ extra where
+            standard = "qcd=123+456-789*0()/"
+            extra = "QCD \ESC\BS\DEL\n"
+
+showbox :: IO ()
+showbox = seqn [writeat (1, y) xs | (y, xs) <- zip [1 .. 13] box]
+
+display :: String -> IO ()
+display xs = writeat (3, 2) "             " >>= \_ ->
+             writeat (3, 2) (reverse $ take 13 (reverse xs))
+
+calc :: String -> IO ()
+calc xs = display xs >>= \_ ->
+          getChar    >>= \c ->
+          (if elem c buttons then
+            process c xs
+           else
+            beep     >>= \_ ->
+            calc xs)
+
+process :: Char -> String -> IO ()
+process c xs
+  | elem c "qQ\ESC" = quit
+  | elem c "dD\BS\DEL" = delete xs
+  | elem c "=\n" = eval xs
+  | elem c "cC" = clear
+  | otherwise = press c xs
+
+quit :: IO ()
+quit = goto (1, 14)
+
+delete :: String -> IO ()
+delete "" = calc ""
+delete xs = calc (init xs)
+
+eval :: String -> IO ()
+eval xs = case parse expr xs of
+                [(n, "")] -> calc (show n)
+                _       -> beep >>= \_ -> calc xs
+
+clear :: IO ()
+clear = calc ""
+
+press :: Char -> String -> IO ()
+press c xs = calc (xs ++ [c])
+
+run :: IO ()
+run = cls     >>= \_ ->
+      showbox >>= \_ ->
+      clear
---
+++ ProgrammingInHaskell-Hutton/ch9/Notes9.hs
@@ -0,0 +1,83 @@
+module Notes9 (
+  seqn,
+  writeat,
+  beep,
+  cls,
+  goto
+) where
+
+import System.IO
+
+-- ** Interactive Programs
+
+-- * The input/output Type
+{- An interactive program can be viewed as a pure function that takes the
+current state of the "world" as its argument, and returns a modified state
+of the world. Additionaly, it might produce a result value besides producing
+side effects.
+
+  type IO a = World -> (a, World)
+
+In reality, Haskell systems don't  pass the entirety of the World's state to
+improve efficiency.
+
+Examples of simple actions:
+
+  getChar :: IO Char
+  putChar :: Char -> IO ()
+  return :: a -> IO a
+-}
+
+-- * Sequencing
+{-
+  (>>=) :: IO a -> (a -> IO b) -> IO b
+  f >>= g = \world -> case f world of
+                        (v, world') -> g v world'
+ -}
+
+-- * Derived Primitives
+getLine' :: IO String
+getLine' = getChar >>= \x ->
+            (if x == '\n'
+              then return []
+             else
+              getLine' >>= \xs ->
+              return (x : xs))
+
+putStr' :: String -> IO ()
+putStr' [] = return ()
+putStr' (x : xs) = putChar x >>= \_ ->
+                   putStr' xs
+
+putStrLn' :: String -> IO ()
+putStrLn' xs = putStr' xs >>= \_ ->
+               putChar '\n'
+
+strlen :: IO ()
+strlen = putStr' "Enter a string: " >>= \_ ->
+         getLine'                   >>= \xs ->
+         putStr' "The string has "  >>= \_ ->
+         putStr' (show (length xs)) >>= \_ ->
+         putStrLn' " characters"
+
+beep :: IO ()
+beep = putStr' "\BEL"
+
+cls :: IO()
+cls = putStr' "\ESC[2J"
+
+type Pos = (Int, Int)
+
+goto :: Pos -> IO ()
+goto (x, y) = putStr' ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+
+writeat :: Pos -> String -> IO ()
+writeat p xs = goto p >>= \_ ->
+               putStr' xs
+
+seqn :: [IO a] -> IO ()
+seqn [] = return ()
+seqn (a : as) = a >>= \_ ->
+                seqn as
+
+putStr'' xs = seqn [putChar x | x <- xs]
