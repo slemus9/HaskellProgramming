deaec53f77b9d7054ef77e7a38a766d31e116bd2
Sebastián Lemus <sebastian.slc9@gmail.com>
2020-01-08 23:10:26 -0500

    ProgrammingInHaskell-Hutton: chapter 10 started

---
+++ ProgrammingInHaskell-Hutton/ch10/Notes10.hs
@@ -0,0 +1,126 @@
+module Notes10(
+  Assoc,
+  find
+) where
+
+-- ** Declaring Types and Classes
+
+-- * Type declarations
+{-
+Type declarations establish synonyms for existing types.
+Previous examples:
+  type String = [Char]
+  type Board = [Pos]
+  type Pos = (Int, Int)
+
+Type declarations cannot be recursive, for example the following
+declaration is NOT allowed:
+
+  type Tree = (Int, [Tree])
+
+Type declarations can also be parameterised:
+
+  type Parser a = String -> [(a, String)]
+  type IO a = World -> (a, World)
+-}
+type Pos = (Int, Int)
+type Assoc k v = [(k, v)]
+
+find :: Eq k => k -> Assoc k v -> v
+find k t = head [v | (k', v) <- t, k == k']
+
+-- * Data declarations
+{-
+Generate a completely new type.
+  data Bool = False | True
+-}
+data Move = Lft | Rght | Up | Down
+
+move :: Move -> Pos -> Pos
+move Lft (x, y)  = (x - 1, y)
+move Rght (x, y) = (x + 1, y)
+move Up (x, y)    = (x, y - 1)
+move Down (x, y)  = (x, y + 1)
+
+moves :: [Move] -> Pos -> Pos
+moves [] p = p
+moves (m : ms) p = moves ms (move m p)
+
+flip :: Move -> Move
+flip Lft  = Rght
+flip Rght = Lft
+flip Up    = Down
+flip Down  = Up
+
+data Shape = Circle Float | Rect Float Float
+
+square :: Float -> Shape
+square n = Rect n n
+
+area :: Shape -> Float
+area (Circle r) = pi * r ^ 2
+area (Rect x y) = x * y
+
+{- Data declarations can also be parameterised, for example:
+  data Maybe a = Nothing | Just a
+-}
+safediv :: Int -> Int -> Maybe Int
+safediv _ 0 = Nothing
+safediv m n = Just $ m `div` n
+
+safehead :: [a] -> Maybe a
+safehead [] = Nothing
+safehead xs = Just $ head xs
+
+-- * Recursive types
+-- Naturals
+data Nat = Zero | Succ Nat
+
+nat2int :: Nat -> Int
+nat2int Zero = 0
+nat2int (Succ n) = 1 + nat2int n
+
+int2nat :: Int -> Nat
+int2nat 0 = Zero
+int2nat n = Succ $ int2nat (n - 1)
+
+add :: Nat -> Nat -> Nat
+-- add m n = int2nat $ nat2int m + nat2int n
+add Zero n = n
+add (Succ m) n = Succ (add m n)
+
+-- Lists
+data List a = Nil | Cons a (List a)
+
+len :: List a -> Int
+len Nil = 0
+len (Cons _ xs) = 1 + len xs
+
+-- Binary Trees
+data Tree a = Leaf a | Node (Tree a) a (Tree a)
+
+t :: Tree Int
+t = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Leaf 9))
+
+occurs :: Eq a => a -> Tree a -> Bool
+occurs m (Leaf n) = m == n
+occurs m (Node l n r) = (m == n) || (occurs m l) || (occurs m r)
+
+flatten :: Tree a -> [a]
+flatten (Leaf n) = [n]
+flatten (Node l n r) = flatten l ++ [n] ++ flatten r
+
+occurs' :: (Eq a, Ord a) => a -> Tree a -> Bool
+occurs' m (Leaf n) = m == n
+occurs' m (Node l n r)
+  | m == n    = True
+  | m < n     = occurs m l
+  | otherwise = occurs m r
+
+{-
+Some examples of various types of Trees:
+  data Tree a = Leaf a | Node (Tree a) (Tree a)
+  data Tree a = Leaf | Node (Tree a) a (Tree a)
+  data Tree a b = Leaf a | Node (Tree a b) b (Tree a b)
+  data Tree a = Node a [Tree a]
+-}
---
+++ ProgrammingInHaskell-Hutton/ch10/TautologyChecker.hs
@@ -0,0 +1,41 @@
+import Notes10
+
+-- * Tautology checker
+data Prop = Const Bool
+            | Var Char
+            | Not Prop
+            | And Prop Prop
+            | Imply Prop Prop
+
+type Subst = Assoc Char Bool
+
+p1 :: Prop
+p1 = And (Var 'A') (Not (Var 'A'))
+
+p2 :: Prop
+p2 = Imply (And (Var 'A') (Var 'B')) (Var 'A')
+
+p3 :: Prop
+p3 = Imply (Var 'A') (And (Var 'A') (Var 'B'))
+
+p4 :: Prop
+p4 = Imply (And (Var 'A') (Imply (Var 'A') (Var 'B'))) (Var 'B')
+
+eval :: Subst -> Prop -> Bool
+eval _ (Const b)   = b
+eval s (Var x)     = find x s
+eval s (Not p)     = not(eval s p)
+eval s (And p q)   = eval s p && eval s q
+eval s (Imply p q) = eval s p <= eval s q
+
+vars :: Prop -> [Char]
+vars (Const _)   = []
+vars (Var x)     = [x]
+vars (Not p)     = vars p
+vars (And p q)   = vars p ++ vars q
+vars (Imply p q) = vars p ++ vars q
+
+bools :: Int -> [[Bool]]
+bools 0 = [[]]
+bools n = map (False:) bss ++ map (True:) bss
+          where bss = bools (n - 1)
